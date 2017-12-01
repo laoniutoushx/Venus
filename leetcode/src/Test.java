c57e112ff4e711ff788aae847618da60a25df6c9
Haruhi <shanghaoxi@live.com>
2017-12-01 08:24:20 +0800

    leetcode

---
+++ leetcode/src/Test.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        String result = "name=haruhi&age=12&index=33&class=sanyi&istrue=t";
+        System.out.println(parseResult(result));
+    }
+
+    /**
+     * 获取下一天的日期
+     * @return
+     */
+    public static Date getNextDay(){
+        try {
+            Thread.currentThread().sleep(24 * 60 * 60 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new Date();
+    }
+
+
+    public static Map<String, String> parseResult(String result){
+        Map<String, String> map = new HashMap<String, String>();
+        if(StringUtils.isNotBlank(result)){
+            String[] keyValue = result.split("&");
+            for(String item:keyValue){
+                String[] it = item.split("=");
+                if(StringUtils.isNotBlank(it[0]) && StringUtils.isNotBlank(it[1])){
+                    map.put(it[0], it[1]);
+                }else{
+                    map.put(it[0], "");
+                }
+            }
+        }
+        return map;
+    }
+}
