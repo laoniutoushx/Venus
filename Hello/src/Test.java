7a657c7d57d32aab5f9799689989769721b16386
Haruhi <shanghaoxi@live.com>
2018-01-29 11:39:35 +0800

    hello

--- Hello/src/Test.java
+++ Hello/src/Test.java
@@ -1,4 +1,8 @@
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Project <h2>Venus</h2>
@@ -9,9 +13,14 @@ import java.math.BigDecimal;
  * @Version v1.0
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(new BigDecimal("2.55").multiply(new BigDecimal(100)).intValue());
-        double re = 2.55 * 100;
-        System.out.println(String.format("%010d", 3));
+    public static void main(String[] args) throws ParseException {
+//        System.out.println(new BigDecimal("2.55").multiply(new BigDecimal(100)).intValue());
+//        double re = 2.55 * 100;
+//        System.out.println(String.format("%010d", 3));
+        Date date = new SimpleDateFormat("yyyyMMddHHmmss").parse("20180124000850");
+        date.setSeconds(0);
+        date.setMinutes(date.getMinutes() - date.getMinutes() % 10);
+        Timestamp tmie = Timestamp.valueOf("2018-01-24 00:08:50");
+        System.out.println(tmie.toString());
     }
 }
